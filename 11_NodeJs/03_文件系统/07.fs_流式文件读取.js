6738647e0c9b5dcd25225099f18599f98e9d3dea
colg <121529654@qq.com>
2018-08-23 14:08:35 +0800

    update -> NodeJS

--- 11_NodeJs/03_文件系统/07.fs_流式文件读取.js
+++ 11_NodeJs/03_文件系统/07.fs_流式文件读取.js
@@ -8,7 +8,7 @@ const fs = require('fs')
 let rs = fs.createReadStream('E:\\Learning\\尚硅谷 www.atguigu.com\\HTML5前端视频\\11、尚硅谷NodeJS视频\\视频\\20.尚硅谷_node基础_fs模块的其他方法.avi')
 
 // 创建一个可写流
-let ws = fs.createWriteStream('E:\\Learning\\尚硅谷 www.atguigu.com\\HTML5前端视频\\11、尚硅谷NodeJS视频\\视频\\z.avi')
+let ws = fs.createWriteStream('20.尚硅谷_node基础_fs模块的其他方法.avi')
 
 // 将可读流中的内容，直接输出到可写流中
 rs.pipe(ws)
