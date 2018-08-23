faac35c02bbc859837309a076355afa10bd18a69
colg <121529654@qq.com>
2018-08-23 11:20:24 +0800

    update -> MongoDB

--- 12_MongoDB/01_mongoose/util/conn_mongo.js
+++ 12_MongoDB/01_mongoose/util/conn_mongo.js
@@ -3,6 +3,4 @@
  */
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/mongoose_test', {useNewUrlParser: true})
-mongoose.connection.once('open', function () {
-    return console.log('数据库连接成功: ' + new Date().toLocaleString())
-})
+mongoose.connection.once('open', () => console.log('数据库连接成功: ' + new Date().toLocaleString()))
