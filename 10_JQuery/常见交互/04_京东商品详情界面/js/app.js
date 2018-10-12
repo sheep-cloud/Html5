b44726d16c0875f90066fdf002bd00947ac0db2a
colg <121529654@qq.com>
2018-10-12 14:57:42 +0800

    add -> 3. 输入搜索关键字, 列表显示匹配的结果

--- 10_JQuery/常见交互/04_京东商品详情界面/js/app.js
+++ 10_JQuery/常见交互/04_京东商品详情界面/js/app.js
@@ -21,6 +21,36 @@
 $(function () {
     showHide()
     hoverSubMenu()
+    search()
+
+    /**
+     * 3. 输入搜索关键字, 列表显示匹配的结果
+     */
+    function search() {
+        // 绑定多个监听事件
+        $('#txtSearch')
+            .on('keyup focus', function () {
+                // 如果输入框有文本才显示列表
+                if (this.value.trim()) {
+                    $('#search_helper').show()
+                }
+            })
+            .blur(function () {
+                // 隐藏列表
+                $('#search_helper').hide()
+            })
+    }
+
+    /**
+     * 2. 鼠标移动切换二级导航菜单的切换显示和隐藏
+     */
+    function hoverSubMenu() {
+        $('#category_items > div').hover(function () {
+            $(this).children(':last').show()
+        }, function () {
+            $(this).children(':last').hide()
+        })
+    }
 
     /**
      * 1. 鼠标移入显示，移出隐藏
@@ -38,14 +68,4 @@ $(function () {
         })
     }
 
-    /**
-     * 2. 鼠标移动切换二级导航菜单的切换显示和隐藏
-     */
-    function hoverSubMenu() {
-        $('#category_items > div').hover(function () {
-            $(this).children(':last').show()
-        }, function () {
-            $(this).children(':last').hide()
-        })
-    }
 })
