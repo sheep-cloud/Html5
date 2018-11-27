9d8e03a51fc51ecf0ad6c454f65f1aa7428ba4b1
colg <121529654@qq.com>
2018-11-27 14:42:45 +0800

    update -> 修改mongoose的models类型

--- 23_Vue-Shop/shop-server/db/models.js
+++ 23_Vue-Shop/shop-server/db/models.js
@@ -17,12 +17,9 @@ mongoose.connection.on('connected', () => console.log(`数据库连接成功：$
 
 // 2. 得到对应特定集合的Model: UserModel
 const userSchema = mongoose.Schema({
-  // 用户名
-  'name': {type: String},
-  // 密码
-  'pwd': {type: String},
-  // 类型
-  'phone': {'type': String}
+  name: String, // 用户名
+  pwd: String, // 密码
+  phone: {type: String} // 手机号码
 })
 UserModel = mongoose.model('user', userSchema)
 
